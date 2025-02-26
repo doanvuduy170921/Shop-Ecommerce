f14539cc50b20415024e50dab479b76240e259b2
Doan Vu Duy <doanvuduyndh@gmail.com>
2025-02-26 11:06:27 +0700

    add base project

---
+++ ShopEcommerce/src/test/java/com/example/ShopEcommerce/ShopEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
